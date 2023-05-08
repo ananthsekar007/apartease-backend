98a04a1b5de0a8133dfe59a2b78ba3d17ae9aca8
ananthsekar007 <ananthsekar007@gmail.com>
2023-05-08 12:00:49 +0530

    [Ananth] Adds Email validation

--- Dao/UserLoginInput.cs
+++ Dao/UserLoginInput.cs
@@ -1,7 +1,10 @@
-﻿namespace apartease_backend.Dao
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apartease_backend.Dao
 {
     public class UserLoginInput
     {
+        [EmailAddress]
         public string Email { get; set;} = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
